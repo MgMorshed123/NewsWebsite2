0238a78ad85225b8e87344f1944215fb1beacf6a
My Name <cpgsom819@gmail.com>
2023-03-12 06:11:36 +0800

    loading data

--- src/NewsSummaryCard/NewsSumaryCard.js
+++ src/NewsSummaryCard/NewsSumaryCard.js
@@ -1,10 +1,54 @@
 import React from 'react';
+import { Image } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
+import { FaRegBookmark, FaShareAlt } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const NewsSumaryCard = ({news}) => {
+
+    const {_id, title , author, details, totla_view, image_url}   = news;
+
+
+
+
     return (
-        <div>
-            <h1>scsc {news.title}</h1>
-        </div>
+        <Card className="text-center">
+        <Card.Header className ='d-flex'>
+            <div>
+                <Image
+                  roundedCircle
+                  src={author.img}
+                  style={{height : '60px'}}
+
+                >
+            
+
+                </Image>
+            </div>
+
+            <div>
+                <FaRegBookmark></FaRegBookmark>
+                <FaShareAlt></FaShareAlt>
+            </div>
+        </Card.Header>
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Img variant='top' src={image_url} />
+          <Card.Text>
+        
+        {
+            details.length > 250 ? 
+
+            <p>{details.slice(0,250) + '...'} <Link  to={`/news/${_id}`}>Read More </Link> </p>
+
+            : <p>{details}</p>
+        }
+     
+          </Card.Text>
+          {/* <Butto variant="primary">Go somewhere</Butto> */}
+        </Card.Body>
+        <Card.Footer className="text-muted">2 days ago</Card.Footer>
+      </Card>
     );
 };
 
